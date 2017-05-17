e076f6a582ef8523f1eefc32c97c0db31e2f8554
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-05-17 12:08:04 +0300

    Refactor main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const {ipcMain} = require('electron');
 
 let mainWindow;
 
-function createWindow() {
+function openWindow() {
     storage.getAll(function(error, data) {
         if (data.contentUrl) {
             openContentWindow(data.contentUrl);
@@ -20,74 +20,78 @@ function createWindow() {
         }
     });
 
-    hotkey.register('global', 'Control+A', 'event-1');
+    registerHotKeys();
+    addHotKeyListeners();
+    addEventListeners();
+}
 
+function registerHotKeys() {
+    hotkey.register('global', 'Control+A', 'open-admin-panel');
+}
+
+function addHotKeyListeners() {
     app.on('shortcut-pressed', (event) => {
-        if (event == 'event-1') {
+        if (event === 'open-admin-panel') {
             openAdminPanel();
         }
     });
+}
 
+function addEventListeners() {
     ipcMain.on('close-admin-panel', (event, contentUrl) => {
         openContentWindow(contentUrl);
     });
+}
 
-    function openAdminPanel() {
-        let filePath = getAdminPanelUrl();
-        let newWindow = createWindow(filePath);
-        closeCurrentWindow();
-        mainWindow = newWindow;
-    }
+function openAdminPanel() {
+    let filePath = getAdminPanelUrl();
+    let newWindow = createWindow(filePath);
+    closeCurrentWindow();
+    mainWindow = newWindow;
+}
 
-    function openContentWindow(contentUrl) {
-        let newWindow = createWindow(contentUrl, {webPreferences: {nodeIntegration: false}});
-        closeCurrentWindow();
-        mainWindow = newWindow;
-        hideCursor(mainWindow);
-    }
+function openContentWindow(contentUrl) {
+    let newWindow = createWindow(contentUrl, {webPreferences: {nodeIntegration: false}});
+    closeCurrentWindow();
+    mainWindow = newWindow;
+    hideCursor(mainWindow);
+}
 
-    function closeCurrentWindow() {
-        if (mainWindow) {
-            mainWindow.close();
-        }
-    }
-
-    function hideCursor(window) {
-        window.webContents.on('did-finish-load', function() {
-            window.webContents.insertCSS('*{ cursor: none !important;}')
-        });
-    }
-
-    function getAdminPanelUrl() {
-        return url.format({
-            pathname: path.join(__dirname, 'index.html'),
-            protocol: 'file:',
-            slashes: true
-        });
-    }
-
-    function createWindow(url, windowOptions = {}) {
-        if (windowOptions.kiosk == undefined) {
-            windowOptions.kiosk = true;
-        }
-
-        let newWindow = new BrowserWindow(windowOptions);
-        newWindow.loadURL(url);
-        newWindow.webContents.openDevTools();
-        return newWindow;
+function closeCurrentWindow() {
+    if (mainWindow) {
+        mainWindow.close();
     }
 }
 
-app.on('ready', createWindow);
+function hideCursor(window) {
+    window.webContents.on('did-finish-load', function() {
+        window.webContents.insertCSS('*{ cursor: none !important;}')
+    });
+}
+
+function getAdminPanelUrl() {
+    return url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+}
+
+function createWindow(url, windowOptions = {}) {
+    if (!windowOptions.kiosk) {
+        windowOptions.kiosk = true;
+    }
+
+    let newWindow = new BrowserWindow(windowOptions);
+    newWindow.loadURL(url);
+    newWindow.webContents.openDevTools();
+    return newWindow;
+}
+
+app.on('ready', openWindow);
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit();
-    }
-});
-
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
     }
 });
