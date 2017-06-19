1321121627fec90925dd8918b5d623dc4c2b8f96
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-19 15:39:35 +0300

    [SCREEN-48] Fix formatting

--- api/venues/list.js
+++ api/venues/list.js
@@ -2,22 +2,22 @@
 
 const dynamoDb = require('./../dynamodb');
 const params = {
-  TableName: process.env.VENUES_TABLE,
+    TableName: process.env.VENUES_TABLE,
 };
 
 module.exports.list = (event, context, callback) => {
-  dynamoDb.scan(params, (error, result) => {
-    if (error) {
-      console.error(error);
-      callback(new Error('Couldn\'t fetch the venues.'));
-      return;
-    }
+    dynamoDb.scan(params, (error, result) => {
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\'t fetch the venues.'));
+            return;
+        }
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items),
-    };
-    callback(null, response);
-  });
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Items),
+        };
+        callback(null, response);
+    });
 };
