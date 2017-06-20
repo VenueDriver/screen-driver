23a039a69a27c34f50f061dfb41b3103ac6bed7a
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-20 15:38:30 +0300

    [SCREEN-48] Install TreeView module

--- client/src/app/venues/venues.module.ts
+++ client/src/app/venues/venues.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from "@angular/core";
 import {VenuesComponent} from "./venues.component";
+import {TreeModule} from "angular2-tree-component/dist/angular2-tree-component";
 
 @NgModule({
-    imports: [],
+    imports: [
+        TreeModule
+    ],
     exports: [
         VenuesComponent
     ],
