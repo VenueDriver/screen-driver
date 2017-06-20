f797ecdf3b7db04ba7a6a7430cb1ac32318c4d2a
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-20 17:01:14 +0300

    [SCREEN-48] Add custom tempalate for tree view

--- client/src/app/venues/venues.module.ts
+++ client/src/app/venues/venues.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {VenuesComponent} from "./venues.component";
-import {TreeModule} from "angular2-tree-component/dist/angular2-tree-component";
+import {TreeModule} from "angular-tree-component/dist/angular-tree-component";
 import {VenuesTreeViewComponent} from "./venues-tree-view/venues-tree-view.component";
 
 @NgModule({
