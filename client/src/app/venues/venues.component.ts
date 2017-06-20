75e7fada7febc84651461e93246d700698773915
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-20 13:31:56 +0300

    [SCREEN-74] Create Venues component

---
+++ client/src/app/venues/venues.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'venues',
+    templateUrl: 'venues.component.html'
+})
+export class VenuesComponent implements OnInit {
+  
+    constructor() { }
+
+    ngOnInit() { }
+    
+}
---
+++ client/src/app/venues/venues.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {VenuesComponent} from "./venues.component";
+
+@NgModule({
+    imports: [],
+    exports: [
+        VenuesComponent
+    ],
+    declarations: [
+        VenuesComponent
+    ]
+})
+export class VenuesModule {
+
+}
