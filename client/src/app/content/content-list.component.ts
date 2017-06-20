f99dc195abad26f7476f7d4e1270891fe6d43db0
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-20 13:31:39 +0300

    [SCREEN-74] Create contentList component

---
+++ client/src/app/content/content-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'content-list',
+    templateUrl: 'content-list.component.html'
+})
+export class ContentListComponent implements OnInit {
+  
+    constructor() { }
+
+    ngOnInit() { }
+    
+}
---
+++ client/src/app/content/content-list.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {ContentListComponent} from "./content-list.component";
+
+@NgModule({
+    imports: [],
+    exports: [
+        ContentListComponent
+    ],
+    declarations: [
+        ContentListComponent
+    ]
+})
+export class ContentListModule {
+
+}
