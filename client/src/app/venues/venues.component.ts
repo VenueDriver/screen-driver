3c1f7c66246490a01b27919a8edfb5664ca521fd
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-21 16:56:15 +0300

    [SCREEN-95] Create add button for venues page

--- client/src/app/venues/venues.component.ts
+++ client/src/app/venues/venues.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'venues',
-    templateUrl: 'venues.component.html'
+    templateUrl: 'venues.component.html',
+    styleUrls: ['venues.component.sass']
 })
 export class VenuesComponent implements OnInit {
+
+    isShowAddVenueForm = false;
 
     constructor() { }
 
     ngOnInit() { }
+
+    showAddVenueForm() {
+        this.isShowAddVenueForm = true;
+    }
 
 }
--- client/src/app/venues/venues.module.ts
+++ client/src/app/venues/venues.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from "@angular/core";
+import {BrowserModule} from '@angular/platform-browser';
 import {VenuesComponent} from "./venues.component";
 import {TreeModule} from "angular-tree-component/dist/angular-tree-component";
 import {VenuesTreeViewComponent} from "./venues-tree-view/venues-tree-view.component";
@@ -6,6 +7,7 @@ import {AddVenueFormComponent} from "./add-venue-form/add-venue-form.component";
 
 @NgModule({
     imports: [
+        BrowserModule,
         TreeModule
     ],
     exports: [
