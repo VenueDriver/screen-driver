b4f72ce9d22f0b8f59152357e38ab1e653f06768
Artem Klots <artemkl@softwareplanetgroup.com>
2017-06-22 17:23:55 +0300

    Fix map import for content service

--- client/src/app/content/content.service.ts
+++ client/src/app/content/content.service.ts
@@ -3,6 +3,8 @@ import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs";
 import {Content} from "./content";
 
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class ContentService {
   readonly contentApiPath = 'api/content';
