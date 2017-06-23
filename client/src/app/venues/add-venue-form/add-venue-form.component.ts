ea875cfac971471b68576a6169f3a8d49cdc1d80
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-23 14:30:51 +0300

    [SCREEN-108] Set venue content URL on create

--- client/src/app/venues/add-venue-form/add-venue-form.component.ts
+++ client/src/app/venues/add-venue-form/add-venue-form.component.ts
@@ -11,6 +11,8 @@ import * as _ from 'lodash';
 export class AddVenueFormComponent implements OnInit {
 
     @Input() venues: any;
+    @Input() content: any;
+
     @Output() cancel = new EventEmitter();
     @Output() submit = new EventEmitter();
 
@@ -38,5 +40,10 @@ export class AddVenueFormComponent implements OnInit {
 
     hasUniqueName() {
         return !_.includes(_.map(this.venues, venue => venue.name), this.newVenue.name);
+    }
+
+    setVenueContent(content) {
+        this.newVenue.content = content;
+        this.newVenue.content_id = content.id;
     }
 }
