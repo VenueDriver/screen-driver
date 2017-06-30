6c4827a062eda0c0709c4168eeea832c08d5a5c0
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-30 19:55:13 +0300

    Add region and stage parameters to deploy script

--- api/docker/deploy.sh
+++ api/docker/deploy.sh
@@ -2,6 +2,8 @@
 
 access_key_id="$1"
 secret_access_key="$2"
+aws_region="${3:-us-east-1}"
+aws_stage="${4:-dev}"
 
 deps_image="screendriver-serverless-deps-image"
 deps_container="screendriver-serverless-deps-container"
@@ -23,9 +25,11 @@ docker run --name "$deps_container" "$deps_image"
 docker commit "$deps_container" "$deps_image"
 docker rm -v "$deps_container"
 
+
+docker build -f docker/Dockerfile.deploy -t "$deploy_image" .
+
 echo
 echo "Deploying project to AWS ..."
 echo
 
-docker build -f docker/Dockerfile.deploy -t "$deploy_image" .
-docker run --rm -e AWS_ACCESS_KEY_ID="$1" -e AWS_SECRET_ACCESS_KEY="$2" "$deploy_image"
+docker run --rm -e AWS_ACCESS_KEY_ID="$1" -e AWS_SECRET_ACCESS_KEY="$2" -e REGION="$aws_region" -e STAGE="$aws_stage" "$deploy_image"
