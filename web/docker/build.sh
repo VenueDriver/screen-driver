4070e780c2f6766405dca4623c758778f179d508
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-06-30 16:50:58 +0300

    Use vars for container and image names

--- web/docker/build.sh
+++ web/docker/build.sh
@@ -3,6 +3,11 @@
 output_dir_name="$1"
 host_api_url="$2"
 
+deps_image="screendriver-web-app-deps-image"
+deps_container="screendriver-web-app-deps-container"
+
+build_image="screendriver-web-app-build-image"
+
 cd ..
 
 if test -z "$output_dir_name"; then
@@ -20,17 +25,17 @@ fi
 echo "Installing dependencies ..."
 echo
 
-docker build -f docker/Dockerfile.deps -t screendriver-web-app-deps-image .
-docker run --name screendriver-web-app-deps-container screendriver-web-app-deps-image
-docker commit screendriver-web-app-deps-container screendriver-web-app-deps-image
-docker rm -v screendriver-web-app-deps-container
+docker build -f docker/Dockerfile.deps -t "$deps_image" .
+docker run --name "$deps_container" "$deps_image"
+docker commit "$deps_container" "$deps_image"
+docker rm -v "$deps_container"
 
 echo
 echo "Building project with API_HOST=$host_api_url ..."
 echo
 
-docker build -f docker/Dockerfile.build -t screendriver-web-app-build-image .
-if docker run --rm -v "$output_dir_name:/app-dist" -e API_HOST=$host_api_url screendriver-web-app-build-image; then
+docker build -f docker/Dockerfile.build -t "$build_image" .
+if docker run --rm -v "$output_dir_name:/app-dist" -e API_HOST=$host_api_url "$build_image"; then
   echo
   echo "Project was built successfully. Dist located in $output_dir_name"
 fi
