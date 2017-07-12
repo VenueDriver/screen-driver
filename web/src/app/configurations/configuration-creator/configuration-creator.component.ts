9ee0569fc0b0792939a585ec829a7ec42c2c16f2
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-12 12:44:53 +0300

    Create components sctructure for configurations module

---
+++ web/src/app/configurations/configuration-creator/configuration-creator.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'configuration-creator',
+    templateUrl: 'configuration-creator.component.html'
+})
+export class ConfigurationCreatorComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
---
+++ web/src/app/configurations/configurations.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'configuration',
+    templateUrl: 'configurations.component.html'
+})
+export class ConfigurationsComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
