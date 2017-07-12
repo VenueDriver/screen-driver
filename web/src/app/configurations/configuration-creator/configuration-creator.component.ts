6721968d2f0f28bcbe0b7c2a34b282aae4fba407
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-12 14:41:55 +0300

    Handle error from server on create config

--- web/src/app/configurations/configuration-creator/configuration-creator.component.ts
+++ web/src/app/configurations/configuration-creator/configuration-creator.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 import {ConfigurationsService} from "../configurations.service";
 import {Configuration} from "../entities/configuration";
+import {NotificationService} from "../../notifications/notification.service";
 
 @Component({
     selector: 'configuration-creator',
@@ -12,12 +13,24 @@ export class ConfigurationCreatorComponent implements OnInit {
 
     private config = new Configuration();
 
-    constructor(private configsService: ConfigurationsService) { }
+    constructor(
+        private configsService: ConfigurationsService,
+        private notificationService: NotificationService) { }
 
     ngOnInit() { }
 
     performSubmit() {
-        this.configsService.createConfiguration(this.config)
-            .subscribe(response => this.created.emit());
+        this.configsService.createConfiguration(this.config).subscribe(
+            response => this.handleResponse(),
+            error => this.handleError()
+        );
+    }
+
+    handleResponse() {
+        this.created.emit();
+    }
+
+    handleError() {
+        this.notificationService.showErrorNotificationBar(`Unable to create config`);
     }
 }
