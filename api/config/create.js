13105de59233223378dcdcdf2eccb3674423c598
Artem Klots <artemkl@softwareplanetgroup.com>
2017-07-12 12:34:22 +0300

    [SCREEN-135] WIP API for create config

---
+++ api/config/create.js
@@ -0,0 +1,15 @@
+'use strict';
+let Config = require('./../entities/config');
+
+const dynamodb = require('../dynamodb');
+const responseHelper = require('../helpers/http_response_helper');
+
+
+module.exports.create = (event, context, callback) => {
+    const data = JSON.parse(event.body);
+
+    let config = new Config(data, dynamodb);
+    config.create()
+        .then(newConfig => callback(null, responseHelper.createSuccessfulResponse(newConfig)))
+        .fail(errorMessage => callback(null, responseHelper.createResponseWithError(500, errorMessage)));
+};
