c9ac95fcd49a48f91feabd81ebfa20fc5a25091e
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-13 12:30:20 +0300

    Format code

--- web/src/app/content/content.service.ts
+++ web/src/app/content/content.service.ts
@@ -8,26 +8,27 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ContentService {
-  readonly contentApiPath = `${environment.apiUrl}/api/content`;
 
-  constructor(private http: Http) { }
+    readonly contentApiPath = `${environment.apiUrl}/api/content`;
 
-  getContent(): Observable<Content[]> {
-    return this.http.get(this.contentApiPath)
-      .map(this.extractData);
-  }
+    constructor(private http: Http) { }
 
-  createContent(content: Content): Observable<Content> {
-    return this.http.post(this.contentApiPath, content)
-        .map(this.extractData);
-  }
+    getContent(): Observable<Content[]> {
+        return this.http.get(this.contentApiPath)
+            .map(this.extractData);
+    }
 
-  updateContent(content: Content): Observable<Content> {
-    return this.http.put(`${this.contentApiPath}/${content.id}`, content)
-        .map(this.extractData);
-  }
+    createContent(content: Content): Observable<Content> {
+        return this.http.post(this.contentApiPath, content)
+            .map(this.extractData);
+    }
 
-  private extractData(res: Response) {
-    return res.json() || { };
-  }
+    updateContent(content: Content): Observable<Content> {
+        return this.http.put(`${this.contentApiPath}/${content.id}`, content)
+            .map(this.extractData);
+    }
+
+    private extractData(res: Response) {
+        return res.json() || { };
+    }
 }
