8feab20439e451409144932d8ad30aac5a890b56
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-13 18:19:50 +0300

    Create side bar with config switcher

--- web/src/app/configurations/configurations-manager/configurations-manager.component.ts
+++ web/src/app/configurations/configurations-manager/configurations-manager.component.ts
@@ -1,20 +1,46 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Configuration} from "../entities/configuration";
+import {ConfigStateHolderService} from "../configuration-state-manager/config-state-holder.service";
 
 @Component({
     selector: 'configurations-manager',
-    templateUrl: 'configurations-manager.component.html'
+    templateUrl: 'configurations-manager.component.html',
+    styleUrls: ['configurations-manager.component.sass']
 })
 export class ConfigurationManagerComponent implements OnInit {
 
     @Input() configs: Configuration[];
     @Output() configSelected: EventEmitter<Configuration> = new EventEmitter();
 
-    constructor() { }
+    activeConfig: Configuration;
+    creationMode = false;
 
-    ngOnInit() { }
+    constructor(private configStateHolderService: ConfigStateHolderService) { }
+
+    ngOnInit() {
+        if (this.configs) {
+            this.activeConfig = this.configs[0];
+        }
+    }
 
     onConfigSelected(config: Configuration) {
         this.configSelected.emit(config);
+        this.activeConfig = config;
+    }
+
+    isActive(config: Configuration): boolean {
+        return this.activeConfig && this.activeConfig.id === config.id;
+    }
+
+    handleConfigCreation() {
+        this.configStateHolderService.reloadConfigs();
+    }
+
+    enableCreationMode() {
+        this.creationMode = true;
+    }
+
+    disableCreationMode() {
+        this.creationMode = false;
     }
 }
