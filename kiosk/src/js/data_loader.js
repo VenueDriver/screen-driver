5770ecc6b0019edfdd3c46f1677ca48c9d185197
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-14 12:22:55 +0300

    Create data loader

---
+++ kiosk/src/js/data_loader.js
@@ -0,0 +1,22 @@
+const {net} = require('electron');
+const PropertiesLoader = require('./properties_loader');
+
+class DataLoader {
+
+    static loadVenues() {
+        let api = PropertiesLoader.getApiEndpoint();
+        let venuesUrl = `${api}/api/venues`;
+        let request = net.request(venuesUrl);
+
+        return new Promise((resolve, reject) => DataLoader.performRequest(request, resolve));
+    }
+
+    static performRequest(request, resolve) {
+        request.on('response', response => {
+            response.on('data', data => resolve(data.toString('utf8')))
+        });
+        request.end();
+    }
+}
+
+module.exports = DataLoader;
