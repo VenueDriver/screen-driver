c45a49d00f0c11e8750a3a24c94383ee19dbe345
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-14 15:16:29 +0300

    Impelement content and configs loading

--- kiosk/src/js/data_loader.js
+++ kiosk/src/js/data_loader.js
@@ -2,20 +2,39 @@
 
 const {net} = require('electron');
 const PropertiesLoader = require('./properties_loader');
+const API = PropertiesLoader.getApiEndpoint();
 
 class DataLoader {
 
     static loadVenues() {
-        let api = PropertiesLoader.getApiEndpoint();
-        let venuesUrl = `${api}/api/venues`;
+        let venuesUrl = `${API}/api/venues`;
         let request = net.request(venuesUrl);
 
-        return new Promise((resolve, reject) => DataLoader.performRequest(request, resolve));
+        return DataLoader.generatePromise(request);
     }
 
-    static performRequest(request, resolve) {
+    static loadContent() {
+        let contentUrl = `${API}/api/content`;
+        let request = net.request(contentUrl);
+
+        return DataLoader.generatePromise(request);
+    }
+
+    static loadConfigs() {
+        let contentUrl = `${API}/api/configs`;
+        let request = net.request(contentUrl);
+
+        return DataLoader.generatePromise(request);
+    }
+
+    static generatePromise(request) {
+        return new Promise((resolve, reject) => DataLoader.performRequest(request, resolve, reject));
+    }
+
+    static performRequest(request, resolve, reject) {
         request.on('response', response => {
-            response.on('data', data => resolve(data.toString('utf8')))
+            response.on('data', data => resolve(data.toString('utf8')));
+            response.on('error', error => reject(error))
         });
         request.end();
     }
