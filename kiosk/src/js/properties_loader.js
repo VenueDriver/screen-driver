8b529ae2d2355234c8acc43c2b9cb3fc472b83e3
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-14 12:22:15 +0300

    Create porpeties loader

---
+++ kiosk/src/js/properties_loader.js
@@ -0,0 +1,11 @@
+const PropertiesReader = require('properties-reader');
+const properties = PropertiesReader(__dirname + '/../../config/app.properties');
+
+class PropertiesLoader {
+
+    static getApiEndpoint() {
+        return properties.get('ScreenDriver.api.endpoint');
+    }
+}
+
+module.exports = PropertiesLoader;
