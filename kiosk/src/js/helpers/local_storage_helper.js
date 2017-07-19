8e3dcd2443d4ecbad8c11329b4829bc32c0f8af0
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-19 16:46:15 +0300

    Implement an old storage to new one converter

--- kiosk/src/js/helpers/local_storage_helper.js
+++ kiosk/src/js/helpers/local_storage_helper.js
@@ -3,7 +3,11 @@
 const storage = require('electron-json-storage');
 
 const StorageNames = {
-    SELECTED_SETTING_STORAGE: 'selected_setting'
+    SELECTED_SETTING_STORAGE: 'selected_setting',
+    SELECTED_SCREEN_STORAGE: 'selectedScreen',
+    SELECTED_GROUP_STORAGE: 'selectedGroup',
+    SELECTED_VENUE_STORAGE: 'selectedVenue',
+    CONTENT_URL_STORAGE: 'contentUrl'
 };
 
 class LocalStorageManager {
@@ -16,6 +20,25 @@ class LocalStorageManager {
 
     static getFromStorage(key, callback) {
         return storage.get(key, callback);
+    }
+
+    static getAllFromStorage(callback) {
+        return storage.getAll(callback);
+    }
+
+    static hasStorage(key, callback) {
+        return storage.has(key, callback);
+    }
+
+    static removeUnusedStorage() {
+        LocalStorageManager.hasStorage(StorageNames.SELECTED_SCREEN_STORAGE, (error, hasKey) => {
+            if (hasKey) {
+                storage.remove(StorageNames.SELECTED_SCREEN_STORAGE);
+                storage.remove(StorageNames.SELECTED_GROUP_STORAGE);
+                storage.remove(StorageNames.SELECTED_VENUE_STORAGE);
+                storage.remove(StorageNames.CONTENT_URL_STORAGE);
+            }
+        });
     }
 }
 
