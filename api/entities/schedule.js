f30ed12ba32e09a7965848c46568efaf3bd45f1d
Artem Klots <artemkl@softwareplanetgroup.com>
2017-07-26 16:47:33 +0300

    Implement schedule updating

--- api/entities/schedule.js
+++ api/entities/schedule.js
@@ -41,6 +41,46 @@ class Schedule {
         }
     }
 
+    update() {
+        let deferred = Q.defer();
+        let params = {
+            TableName: process.env.SCHEDULES_TABLE,
+            Key: {
+                id: this.id,
+            },
+            ExpressionAttributeNames: {
+                '#rev': '_rev',
+            },
+            ExpressionAttributeValues: {
+                ':setting_id': this.setting_id,
+                ':cron': this.cron,
+                ':startDate': this.startDate,
+                ':rev': this._rev,
+                ':new_rev': ++this._rev,
+            },
+            UpdateExpression: 'SET setting_id = :setting_id, cron = :cron, startDate= :startDate, #rev = :new_rev',
+            ConditionExpression: "#rev = :rev",
+            ReturnValues: 'ALL_NEW',
+        };
+
+        if (!this._rev) deferred.reject('Missed revision number');
+
+        this.validate(deferred.reject);
+        _updateInDatabase(params);
+
+        return deferred.promise;
+
+        function _updateInDatabase(updateParameters) {
+            db.update(updateParameters, (error, result) => {
+                if (error) {
+                    deferred.reject(error.message);
+                } else {
+                    deferred.resolve(result.Attributes);
+                }
+            });
+        }
+    }
+
     /**
      * Validates schedule. Important: don't validates cron format syntax
      * @param {function} errorCallback: calls with error message attribute if validation failed
@@ -62,10 +102,6 @@ class Schedule {
 
     generateId() {
         this.id = uuid.v1();
-    };
-
-    increaseRevision() {
-        this._rev++;
     };
 }
 
