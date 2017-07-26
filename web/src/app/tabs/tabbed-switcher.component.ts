41488329e84d8bfcc194f822bfb2cd9131b71309
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-26 12:22:03 +0300

    Made first tab active after view init

--- web/src/app/tabs/tabbed-switcher.component.ts
+++ web/src/app/tabs/tabbed-switcher.component.ts
@@ -1,19 +1,25 @@
-import {Component, OnInit, ContentChildren, QueryList, Input} from '@angular/core';
+import {Component, OnInit, ContentChildren, QueryList, Input, AfterViewInit, NgZone} from '@angular/core';
 import {SingleTabComponent} from "./tab/single-tab.component";
 
 @Component({
     selector: 'tabbed-switcher',
     templateUrl: 'tabbed-switcher.component.html'
 })
-export class TabbedSwitcherComponent implements OnInit {
+export class TabbedSwitcherComponent implements OnInit, AfterViewInit {
 
     @ContentChildren(SingleTabComponent) tabs: QueryList<SingleTabComponent>;
 
     @Input() tabTitles: Array<string>;
 
-    constructor() { }
+    constructor(private zone : NgZone) { }
 
     ngOnInit() { }
+
+    ngAfterViewInit() {
+        this.zone.onMicrotaskEmpty.subscribe(() => {
+            this.tabs.first.show = true;
+        });
+    }
 
     switchTab(tabIndex: number) {
         this.tabs.forEach((tab: SingleTabComponent, index) => {
