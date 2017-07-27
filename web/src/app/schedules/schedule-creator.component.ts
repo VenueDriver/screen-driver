ad1b45a8e1f4b4757efc2b788191dcbd214c16eb
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-07-27 14:51:14 +0300

    Add time selectors to schedule creator component

--- web/src/app/schedules/schedule-creator.component.ts
+++ web/src/app/schedules/schedule-creator.component.ts
@@ -2,13 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'schedule-creator',
-    templateUrl: 'schedule-creator.component.html'
+    templateUrl: 'schedule-creator.component.html',
+    styleUrls: ['schedule-creator.component.sass']
 })
 export class ScheduleCreatorComponent implements OnInit {
 
     date = new Date();
+    timeItems: Array<string> = [];
+    timePeriods = ['AM', 'PM'];
 
-    constructor() { }
+    constructor() {
+        this.generateTimeItems();
+    }
+
+    generateTimeItems() {
+        for (let i = 1; i <= 12; i++) {
+            this.timeItems.push(`${i}:00`, `${i}:30`);
+        }
+    }
 
     ngOnInit() { }
 
--- web/src/app/schedules/schedules.module.ts
+++ web/src/app/schedules/schedules.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from "@angular/core";
 import {DatepickerModule} from "angular2-material-datepicker";
 import {ScheduleCreatorComponent} from "./schedule-creator.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {AutocompleteModule} from "../autocomplete/autocomplete.module";
 
 @NgModule({
     imports: [
-        DatepickerModule
+        BrowserModule,
+        DatepickerModule,
+        AutocompleteModule
     ],
     exports: [
         ScheduleCreatorComponent
