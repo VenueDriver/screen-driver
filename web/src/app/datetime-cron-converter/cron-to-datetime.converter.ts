53ff08bbae3752084ab6cd581a993d2d1348896e
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-03 14:18:17 +0300

    Add checks for empty cron in cron to date converter

--- web/src/app/datetime-cron-converter/cron-to-datetime.converter.ts
+++ web/src/app/datetime-cron-converter/cron-to-datetime.converter.ts
@@ -1,6 +1,9 @@
 export class CronToDatetimeConverter {
 
-    static getDateFromCron(cron: string) {
+    static getDateFromCron(cron: string): Date {
+        if (!cron) {
+            return new Date();
+        }
         let parts = cron.split(' ');
         let year = +parts[6];
         let month = parts[4];
@@ -8,12 +11,19 @@ export class CronToDatetimeConverter {
         return new Date(`${month} ${dayOfMonth}, ${year}`);
     }
 
-    static getMinutesFromCron(cron: string): number {
+    static getMinutesFromCron(cron: string): string {
+        if (!cron) {
+            return '';
+        }
         let parts = cron.split(' ');
-        return +parts[1];
+        let minutes = parts[1];
+        return +minutes < 10 ? `0${minutes}` : minutes;
     }
 
     static getHoursFromCron(cron: string): number {
+        if (!cron) {
+            return 0;
+        }
         let parts = cron.split(' ');
         return +parts[2];
     }
