16e9b8389f6456e879070b2e264e7563841b6140
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-04 16:17:44 +0300

    Create getPropertyName function for periodicity

--- web/src/app/enums/periodicity.ts
+++ web/src/app/enums/periodicity.ts
@@ -4,3 +4,7 @@ export const Periodicity = {
     ONE_TIME_EVENT: 'One time event',
     WEEKLY: 'Weekly'
 };
+
+export const getPropertyName = (value: string) => {
+    return _.keys(Periodicity).filter(k => Periodicity[k] === value)[0];
+};
