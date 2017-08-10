17e28648806b7505d1a766cd9f4b4ee9872c9334
Artem Klots <artemkl@softwareplanetgroup.com>
2017-08-10 16:13:52 +0300

    Fix: add change missed on merge

--- api/schedule/list.js
+++ api/schedule/list.js
@@ -2,11 +2,13 @@
 
 const dbHelper = require('./../helpers/db_helper');
 const responseHelper = require('../helpers/http_response_helper');
+const Schedule = require('./entities/schedule');
 
 module.exports.list = (event, context, callback) => {
     dbHelper.findAll(process.env.SCHEDULES_TABLE)
         .then(result => {
-            callback(null, responseHelper.createSuccessfulResponse(result));
+            let schedules = result.map(schedule => new Schedule(schedule));
+            callback(null, responseHelper.createSuccessfulResponse(schedules));
         })
         .fail(error => callback(null, responseHelper.createResponseWithError(500, error)));
 };
