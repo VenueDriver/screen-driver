275a1dcadfd8d0d17d24f106dc813349c748e4a5
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-10 09:48:53 +0300

    Implement updating of settings that contains configs for removed venue items

---
+++ api/setting/helpers/setting_utils.js
@@ -0,0 +1,33 @@
+const dbHelper = require('./../../helpers/db_helper');
+const BulkUpdater = require('./bulk_updater');
+const _ = require('lodash');
+
+module.exports = class SettingUtils {
+
+    static getSettingsWithUpdatedConfig(itemIds, resolve, reject) {
+        let settingsList = [];
+        dbHelper.findAll(process.env.SETTINGS_TABLE)
+            .then(settings => _.forEach(settings, s => {
+                let settingToUpdate = SettingUtils._deleteConfigOfRemovedItems(s, itemIds);
+                if (Object.keys(s.config).length !== Object.keys(settingToUpdate.config).length) {
+                    settingsList.push(settingToUpdate);
+                }
+                resolve(settingsList);
+            }));
+    }
+
+    static _deleteConfigOfRemovedItems(setting, itemIds) {
+        let settingToUpdate = _.clone(setting);
+        _.forEach(itemIds, id => {
+            if (setting.config[id]) {
+                delete settingToUpdate.config[id];
+            }
+        });
+        return settingToUpdate;
+    }
+
+    static updateConfigs(itemIds) {
+        return new Promise((resolve, reject) => SettingUtils.getSettingsWithUpdatedConfig(itemIds, resolve, reject))
+            .then(settingsToUpdate => BulkUpdater.performBulkUpdate(settingsToUpdate));
+    }
+};
