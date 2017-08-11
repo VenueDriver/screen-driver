a7281be635be5dd3b6b1b0f8fddbc7c13ac4e4be
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-11 13:01:30 +0300

    Implement skipping notification if disabled schedule was updated

--- api/schedule/stream/stream_handler.js
+++ api/schedule/stream/stream_handler.js
@@ -1,11 +1,34 @@
 'use strict';
 
 const FunctionInvokeHelper = require('../../helpers/function_invoke_helper');
+const _ = require('lodash');
 
 module.exports.handleEvent = (event, context) => {
+    let changedSchedule = _.find(event.Records, record => {
+        return shouldBeNotified(record);
+    });
+    if (changedSchedule) {
+        triggerUpdateEvent(context);
+    }
+};
+
+function shouldBeNotified(record) {
+    if (record.eventName === 'MODIFY') {
+        return isEnabledScheduleChanged(record.dynamodb);
+    }
+    return true;
+}
+
+function isEnabledScheduleChanged(streamInfo) {
+    let oldSchedule = streamInfo.OldImage;
+    let newSchedule = streamInfo.NewImage;
+    return !(!newSchedule.enabled.BOOL && !oldSchedule.enabled.BOOL);
+}
+
+function triggerUpdateEvent(context) {
     let params = buildParamsForInvokeFunction(context);
     FunctionInvokeHelper.invokeFunction(params, context);
-};
+}
 
 function buildParamsForInvokeFunction(context) {
     let functionName = context.functionName;
