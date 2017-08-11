d9b211f2b3ef3c9f689f8692810dc1ec14422911
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-11 12:29:40 +0300

    Remove callback from function invoke helper

--- api/helpers/function_invoke_helper.js
+++ api/helpers/function_invoke_helper.js
@@ -3,10 +3,9 @@
 const aws = require('aws-sdk');
 const lambda = new aws.Lambda();
 
-module.exports.invokeFunction = (params, context, callback) => {
+module.exports.invokeFunction = (params, context) => {
 
     lambda.invoke(params, (err, data) => {
-        callback(null, err ? err : data);
     })
 
 };
