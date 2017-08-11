eade09963446dfbca63e6a0b562e2543b1f66ac1
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-11 12:34:15 +0300

    Implement notification if name, priority or config of setting changed only

--- api/setting/stream/stream_handler.js
+++ api/setting/stream/stream_handler.js
@@ -3,10 +3,29 @@
 const FunctionInvokeHelper = require('../../helpers/function_invoke_helper');
 const dbHelper = require('./../../helpers/db_helper');
 const PriorityTypes = require('../../entities/priority_types');
+const _ = require('lodash');
 
 module.exports.handleEvent = (event, context) => {
-    triggerUpdateEvent(context);
+    let changedSetting = _.find(event.Records, r => {
+        let result = r.dynamodb;
+        return isSettingChanged(result.OldImage, result.NewImage);
+    });
+    if (changedSetting) {
+        triggerUpdateEvent(context);
+    }
 };
+
+function isSettingChanged(oldSetting, newSetting) {
+    if (!_.isEqual(oldSetting.priority, newSetting.priority)) {
+        return true;
+    }
+    if (!_.isEqual(oldSetting.enabled, newSetting.enabled)) {
+        return true;
+    }
+    let oldConfigKeys = Object.keys(oldSetting.config.M);
+    let newConfigKeys = Object.keys(newSetting.config.M);
+    return !_.isEqual(oldConfigKeys.sort(), newConfigKeys.sort());
+}
 
 function triggerUpdateEvent(context) {
     let priorityTypes = PriorityTypes.getTypes();
