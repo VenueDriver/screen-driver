18b7a4a13ec6be06710bfb77ed91624358653986
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-22 13:49:35 +0300

    Test additional cases for schedule overflow identifier

--- api/test/schedule_overflow_inspector.js
+++ api/test/schedule_overflow_inspector.js
@@ -22,5 +22,37 @@ describe('schedule_overflow_inspector', () => {
 
             expect(result).to.equal(true);
         });
+
+        it('Should return false if two schedules have different set of weekdays', () => {
+            let firstSchedule = {eventCron: '0 0 8 * * MON,TUE,WED'};
+            let secondSchedule = {eventCron: '0 0 9 * * THU,FRI,SAT'};
+            let result = ScheduleOverflowInspector.areIntersect(firstSchedule, secondSchedule);
+
+            expect(result).to.equal(false);
+        });
+
+        it('Should return true if two schedules have one common weekday in a set of weekdays', () => {
+            let firstSchedule = {eventCron: '0 0 8 * * MON,TUE,WED'};
+            let secondSchedule = {eventCron: '0 0 9 * * THU,TUE,SAT'};
+            let result = ScheduleOverflowInspector.areIntersect(firstSchedule, secondSchedule);
+
+            expect(result).to.equal(true);
+        });
+
+        it('Should return true if two schedules have two common weekdays in a set of weekdays', () => {
+            let firstSchedule = {eventCron: '0 0 8 * * MON,TUE,SAT'};
+            let secondSchedule = {eventCron: '0 0 9 * * THU,TUE,SAT'};
+            let result = ScheduleOverflowInspector.areIntersect(firstSchedule, secondSchedule);
+
+            expect(result).to.equal(true);
+        });
+
+        it('Should return true if two schedules have the same set of weekdays', () => {
+            let firstSchedule = {eventCron: '0 0 8 * * MON,TUE,SAT'};
+            let secondSchedule = {eventCron: '0 0 9 * * MON,TUE,SAT'};
+            let result = ScheduleOverflowInspector.areIntersect(firstSchedule, secondSchedule);
+
+            expect(result).to.equal(true);
+        });
     });
 });
