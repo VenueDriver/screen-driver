15432c9f54382650250166d09371dea523a1c2bb
VitaliiKobrin <vitaliik@softwareplanet.uk.com>
2017-08-23 17:07:57 +0300

    Test conflicts in occasional settings

--- api/test/update_setting.js
+++ api/test/update_setting.js
@@ -407,6 +407,51 @@ describe('update_setting', () => {
             });
     });
 
+    it('Should update one time setting if content URL coincides in conflicted settings', () => {
+        let config = {screen_id: 'content_id', screen_id_2: 'content_id_2'};
+        let cronExpressions = {
+            existingEventCron: '0 0 8 1 JAN * 2017',
+            existingEndEventCron: '0 0 8 2 JAN * 2017',
+            newEventCron: '0 0 8 1 JAN * 2017',
+            newEndEventCron: '0 0 8 2 JAN * 2017',
+        };
+        return TestDataSever.saveOccasionalSettingsWithSchedules(cronExpressions, config)
+            .then((newSetting) => {
+                let updatedConfig = {screen_id: 'content_id'};
+                let updatedSetting = SettingDataPreparationHelper.getOccasionalSetting('New Year', updatedConfig);
+                return MultiOperationHelper.update({body: JSON.stringify(newSetting)}, updatedSetting);
+            })
+            .then(updatedSetting => {
+                let expectation = (body, response) => {
+                    expect(body).to.have.property('_rev').that.equal(1);
+                    expect(response).to.have.property('statusCode').that.equal(200);
+                };
+                return MultiOperationHelper.test(updatedSetting, expectation);
+            });
+    });
+
+    it('Should update one time setting with enabled status if content URL coincides in conflicted settings', () => {
+        let config = {screen_id: 'content_id', screen_id_2: 'content_id_2'};
+        let cronExpressions = {
+            existingEventCron: '0 0 8 1 JAN * 2017',
+            existingEndEventCron: '0 0 8 2 JAN * 2017',
+            newEventCron: '0 0 8 1 JAN * 2017',
+            newEndEventCron: '0 0 8 2 JAN * 2017',
+        };
+        return TestDataSever.saveOccasionalSettingsWithSchedules(cronExpressions, config)
+            .then((newSetting) => {
+                let updatedConfig = {screen_id: 'content_id'};
+                let updatedSetting = SettingDataPreparationHelper.getOccasionalSetting('New Year', updatedConfig);
+                return MultiOperationHelper.update({body: JSON.stringify(newSetting)}, updatedSetting);
+            })
+            .then(updatedSetting => {
+                let expectation = (body) => {
+                    expect(body).to.have.property('enabled').that.equal(true);
+                };
+                return MultiOperationHelper.test(updatedSetting, expectation);
+            });
+    });
+
     it('Shouldn\'t update setting without name', () => {
         let newSetting = {name: 'New Year'};
         let updatedSetting = {_rev: 0};
