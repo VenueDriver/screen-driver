14e15e6614ae97a8a9c3474b363bd8e107924bd9
Artem Klots <artemkl@softwareplanetgroup.com>
2017-08-30 15:19:52 +0300

    Code style changes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
-import {NgModule}              from '@angular/core';
-import {RouterModule, Routes}  from '@angular/router';
-
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 import {UsersManagementComponent} from "./users-management/users-management.component";
 import {ContentManagementComponent} from "./content-management/content-management.component";
 
