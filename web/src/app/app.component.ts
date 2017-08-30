e956c65e99d4ffd6723c33a885548448c419989e
Artem Klots <artemkl@softwareplanetgroup.com>
2017-08-30 15:30:18 +0300

    Hide sidebar on users page

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import {Component, OnInit} from '@angular/core';
+import {Router, RoutesRecognized} from "@angular/router";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
     title = 'app';
+    isSidebarHidden: boolean;
 
-    constructor() {}
+    constructor(private route: Router) {
+    }
 
+    ngOnInit() {
+        this.route.events.subscribe((data) => {
+            if (data instanceof RoutesRecognized) {
+                this.isSidebarHidden = data.state.root.firstChild.data['isSidebarHidden'];
+            }
+        });
+    }
 }
