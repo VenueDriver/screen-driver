65876c7db9678a104328705a07fc08701686dff0
Artem Klots <artemkl@softwareplanetgroup.com>
2017-08-31 17:24:01 +0300

    Implement users list

--- web/src/app/users-management/users.service.ts
+++ web/src/app/users-management/users.service.ts
@@ -1,17 +1,22 @@
 import {Injectable} from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
 import {NotificationService} from "../notifications/notification.service";
 import {User} from "../auth/user";
 import {environment} from "../../environments/environment";
-import {Subject} from "rxjs";
+import {Subject, Observable, BehaviorSubject} from "rxjs";
 
 const USERS_API = `${environment.apiUrl}/api/users`;
 
 @Injectable()
 export class UsersService {
+    createUserEvent: Subject<User> = new Subject();
 
     constructor(private http: Http,
                 private notificationService: NotificationService,) {
+    }
+
+    loadUsers(): Observable<Response> {
+        return this.http.get(USERS_API)
     }
 
     create(user: User) {
@@ -20,7 +25,8 @@ export class UsersService {
             .map(response => response.json())
             .subscribe(
                 response => {
-                    subject.next(response)
+                    subject.next(response);
+                    this.createUserEvent.next(response);
                 },
                 error => {
                     let errorMessage = error.json().message;
@@ -31,4 +37,8 @@ export class UsersService {
         return subject;
     }
 
+    //TODO Implement it
+    update(user: User) {
+
+    }
 }
