689afa98afe807b598390c1d1edf08fdf7c34d5f
Artem Klots <artemkl@softwareplanetgroup.com>
2017-09-04 15:08:45 +0300

    Remove unused auth constants

--- web/src/app/auth/auth-consts.ts
+++ web/src/app/auth/auth-consts.ts
@@ -1,8 +1,3 @@
-export const SIGN_IN = 'SIGN_IN';
-export const AUTH_ERROR = 'AUTH_ERROR';
-export const IN_PROCESS = 'IN_PROCESS';
-export const UNREGISTERED_USER = 'UNREGISTERED_USER';
-
 export const ID_TOKEN_PARAM = 'id_token';
 export const ROLLBACK_URL_PARAM = 'rollbackUrl';
 
