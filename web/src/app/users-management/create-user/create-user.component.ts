b14c9aea8132bdcc8ca7fffd837de454ed867b55
Artem Klots <artemkl@softwareplanetgroup.com>
2017-09-06 14:27:13 +0300

    Add email uniquness validation

--- web/src/app/users-management/create-user/create-user.component.ts
+++ web/src/app/users-management/create-user/create-user.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
 import {User} from "../../auth/user";
+import {UsersService} from "../users.service";
+import {Subscription} from "rxjs";
+import {NgModel} from "@angular/forms";
 
 @Component({
     selector: 'create-user',
@@ -10,21 +13,40 @@ export class CreateUserComponent implements OnInit {
     @Output() cancel = new EventEmitter();
     @Output() submit = new EventEmitter();
     user: User = new User();
-    isShowEmailValidationError: boolean = false;
+    users: Array<User> = [];
 
-    constructor() {
+    constructor(private usersService: UsersService) {
     }
 
     ngOnInit() {
+        this.users = this.usersService.getUsers();
     }
 
-    validateEmail() {
-        this.isShowEmailValidationError = !this.isEmailValid();
+    validateEmail(emailModel: NgModel) {
+        !this.isEmailValid(emailModel) || !this.isEmailUnique(emailModel);
     }
 
-    isEmailValid(): boolean {
+    isEmailValid(emailModel?: NgModel): boolean {
         let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regex.test(this.user.email)
+        let validationResult = regex.test(this.user.email);
+        if (emailModel && !validationResult) {
+            emailModel.control.setErrors({notValid: true});
+        }
+        return validationResult;
+    }
+
+    isEmailUnique(emailModel: NgModel) {
+        let emails = this.users.map(user => user.email);
+        let validationResult = !emails.find(email => email === this.user.email);
+        if (!validationResult) {
+            emailModel.control.setErrors({notUnique: true});
+        }
+        return validationResult;
+    }
+
+    getValidationMessage(emailModel: NgModel): string {
+        if (emailModel.control.hasError('notValid')) return 'Invalid Email';
+        if (emailModel.control.hasError('notUnique')) return 'This email is already in use';
     }
 
     setUserRole(event: boolean) {
