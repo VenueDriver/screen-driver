ac6e35d053892b77b1b85b0d49a60c5f3a82cfe6
Artem Klots <artemkl@softwareplanetgroup.com>
2017-09-13 15:46:19 +0300

    Add edit email form

--- web/src/app/profile-management/user-profile/user-profile.component.ts
+++ web/src/app/profile-management/user-profile/user-profile.component.ts
@@ -8,11 +8,24 @@ import {User} from "../../user/user";
 })
 export class UserProfileComponent {
     @Input() user: User;
+    isEditEmailMode: boolean = false;
 
     constructor() {
     }
 
     getUserRole(): string {
         return this.user.isAdmin ? 'Administrator' : 'Operator';
+    }
+
+    changeEditEmailMode(flag: boolean) {
+        this.isEditEmailMode = flag;
+    }
+
+    performCancel() {
+        this.changeEditEmailMode(false);
+    }
+
+    performSubmit() {
+
     }
 }
