978d27913bb22b99bd2553321271db6cf8dffbed
Artem Klots <artemkl@softwareplanetgroup.com>
2017-09-14 15:38:18 +0300

    Fix sign out

--- api/src/auth/sign_out.js
+++ api/src/auth/sign_out.js
@@ -2,9 +2,13 @@
 
 const UserPool = require('../user_pool/user_pool');
 const ResponseHelper = require('../helpers/http_response_helper');
+const TokenParser = require('../auth_token/auth_token_parser');
 
 module.exports.handler = (event, context, callback) => {
     const userDetails = JSON.parse(event.body);
+    let token = event.headers.authorization.replace('Bearer ', '');
+    let decodedToken = TokenParser.decodeToken(token);
+    userDetails.username = decodedToken.payload['cognito:username'];
 
     UserPool.signOut(userDetails);
 
