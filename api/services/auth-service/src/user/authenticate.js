9beeb72fcb735d4e1c36236fd44ca4ac883da8ad
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-10-10 18:01:42 +0300

    Implement loading user from Cognito User Pool if user not found in Users table

--- api/services/auth-service/src/user/authenticate.js
+++ api/services/auth-service/src/user/authenticate.js
@@ -2,16 +2,34 @@
 
 const DbHelper = require('../helpers/db_helper');
 const UserPool = require('../user_pool/user_pool');
+const UserDetailsLoader = require('../user_pool/user_details_loader');
+const UserSaver = require('../user/helpers/user_saver');
 
 module.exports.authenticate = (userDetails) => {
-    return loadUser(userDetails);
+    return loadUser(userDetails)
+        .then(userDetails => UserPool.authenticate(userDetails))
 };
 
 function loadUser(userDetails) {
     let params = buildFindUserByEmailParams(userDetails.email);
     return DbHelper.findByParams(params).then(users => {
-        userDetails.username = users[0].username;
-        return UserPool.authenticate(userDetails);
+        if (users[0]) {
+            userDetails.username = users[0].username;
+            return new Promise((resolve, reject) => resolve(userDetails));
+        }
+        return syncUserProfile(userDetails);
+    });
+}
+
+function syncUserProfile(userDetails) {
+    userDetails.username = userDetails.email;
+    return new Promise((resolve, reject) => {
+        UserDetailsLoader.loadUserByUsername(userDetails.username)
+            .then(cognitoUser => {
+                UserSaver.saveExistentCognitoUser(cognitoUser)
+                    .then(result => resolve(userDetails))
+                    .catch(error => reject(error));
+            });
     });
 }
 
