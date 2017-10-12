b533bddaffcccc3ac40b545fefda8cf1c14a12e6
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-10-12 10:06:37 +0300

    Update user status in DB after disabling in cognito

--- api/services/auth-service/src/user/helpers/user_disable_helper.js
+++ api/services/auth-service/src/user/helpers/user_disable_helper.js
@@ -2,15 +2,31 @@
 
 const UserPool = require('../../user_pool/user_pool');
 const DbHelper = require('../../helpers/db_helper');
+const ParametersBuilder = require('./parameters_builder');
 
 module.exports.disableUser = (userId, issuer) => {
     return new Promise((resolve, reject) => {
         if (userId === issuer.id) {
             reject('Unable to perform operation');
         }
+        let userToDisable;
+        let responseFromUserPool;
         DbHelper.findOne(process.env.USERS_TABLE, userId)
-            .then(user => UserPool.disableUser(user.username))
-            .then(response => resolve(response))
+            .then(user => {
+                userToDisable = user;
+                return UserPool.disableUser(user.username);
+            })
+            .then(response => {
+                responseFromUserPool = response;
+                return updateUserStatus(userToDisable);
+            })
+            .then(() => resolve(responseFromUserPool))
             .catch(error => reject(error));
     });
 };
+
+function updateUserStatus(user) {
+    user.enabled = false;
+    let params = ParametersBuilder.buildUpdateUserStatusRequestParameters(user);
+    return DbHelper.updateItem(params);
+}
