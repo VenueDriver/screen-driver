a80ebe8d2fd0cf10b32153f0fc71dd05e61e1871
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-10-12 08:56:25 +0300

    Validate user before disabling to prevent changing status of himselve

--- api/services/auth-service/src/user/disable.js
+++ api/services/auth-service/src/user/disable.js
@@ -3,11 +3,13 @@
 const ModulePathManager = require('../module_path_manager');
 const responseHelper = require(ModulePathManager.getBasePath() + 'lib/helpers/http_response_helper');
 const UserDisableHelper = require('./helpers/user_disable_helper');
+const TokenParser = require(ModulePathManager.getBasePath() + 'lib/auth_token/auth_token_parser');
 
 module.exports.handler = (event, context, callback) => {
     let userId = event.pathParameters.id;
+    let currentUserDetails = TokenParser.getCurrentUserDetails(event.headers['Authorization']);
 
-    UserDisableHelper.disableUser(userId)
+    UserDisableHelper.disableUser(userId, currentUserDetails)
         .then(newUser => callback(null, responseHelper.createSuccessfulResponse(newUser)))
         .catch(errorMessage => callback(null, responseHelper.createResponseWithError(500, errorMessage)));
 };
--- api/services/auth-service/src/user/helpers/user_disable_helper.js
+++ api/services/auth-service/src/user/helpers/user_disable_helper.js
@@ -3,8 +3,11 @@
 const UserPool = require('../../user_pool/user_pool');
 const DbHelper = require('../../helpers/db_helper');
 
-module.exports.disableUser = (userId) => {
+module.exports.disableUser = (userId, issuer) => {
     return new Promise((resolve, reject) => {
+        if (userId === issuer.id) {
+            reject('Unable to perform operation');
+        }
         DbHelper.findOne(process.env.USERS_TABLE, userId)
             .then(user => UserPool.disableUser(user.username))
             .then(response => resolve(response))
