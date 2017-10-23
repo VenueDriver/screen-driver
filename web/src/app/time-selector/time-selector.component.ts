97a4d4925134e2409831a324595931b2821b2085
Artem Klots <artemkl@softwareplanetgroup.com>
2017-10-23 13:17:08 +0300

    Implement time selector as a component

---
+++ web/src/app/time-selector/time-selector.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+    selector: 'time-selector',
+    templateUrl: './time-selector.component.html',
+    styleUrls: ['./time-selector.component.sass']
+})
+export class TimeSelectorComponent implements OnInit {
+    @Input() label: string;
+    @Input() defaultTime: string = '12:00';
+    @Input() defaultTimePeriod: string = 'AM';
+
+    @Output() timeChange = new EventEmitter();
+    @Output() timePeriodChange = new EventEmitter();
+
+    timePeriods = ['AM', 'PM'];
+    timeItems: Array<string> = [];
+    time: string;
+
+    constructor() {
+        this.generateTimeItems();
+    }
+
+    ngOnInit() {
+    }
+
+    generateTimeItems() {
+        this.timeItems.push(`${12}:00`, `${12}:15`, `${12}:30`, `${12}:45`);
+        for (let i = 1; i < 12; i++) {
+            this.timeItems.push(`${i}:00`, `${i}:15`, `${i}:30`, `${i}:45`);
+        }
+    }
+
+    changeTimePeriod() {
+        if (this.defaultTimePeriod === this.timePeriods[0]) {
+            this.timePeriodChange.emit(this.timePeriods[1])
+        } else {
+            this.timePeriodChange.emit(this.timePeriods[0]);
+        }
+    }
+
+    setTime(time: string) {
+        this.timeChange.emit(time);
+    }
+}
