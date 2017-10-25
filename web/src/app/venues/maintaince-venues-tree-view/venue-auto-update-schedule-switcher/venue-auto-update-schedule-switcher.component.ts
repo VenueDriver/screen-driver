203ea74de0ba7936f8afbea2c2c27a04d14b30c7
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-25 15:13:29 +0300

    Add empty component for venue-auto-update-schedule configuration

---
+++ web/src/app/venues/maintaince-venues-tree-view/venue-auto-update-schedule-switcher/venue-auto-update-schedule-switcher.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import * as _ from 'lodash';
+
+@Component({
+    selector: 'auto-update-schedule-switcher-venues-tree-view',
+    templateUrl: 'venue-auto-update-schedule-switcher.component.html',
+    styleUrls: ['./venue-auto-update-schedule-switcher.component.sass']
+})
+export class VenueAutoUpdateScheduleSwitcherComponent {
+
+    constructor(
+    ) { }
+
+}
