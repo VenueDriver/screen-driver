38ca8b22eb810a7dced0b5074c80bd37b0bdf810
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-26 14:58:17 +0300

    Add input and output for AutoupdateSchedule switcher

--- web/src/app/maintenance/maintaince-venues-tree-view/venue-auto-update-schedule-switcher/venue-auto-update-schedule-switcher.component.ts
+++ web/src/app/maintenance/maintaince-venues-tree-view/venue-auto-update-schedule-switcher/venue-auto-update-schedule-switcher.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import * as _ from 'lodash';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {AutoupdateSchedule} from "../../entities/autoupdate-schedule";
+import {CronConvertStrategy, CustomCronConverter} from "../../../core/utils/custom-cron-parser";
 
 @Component({
     selector: 'auto-update-schedule-switcher',
@@ -8,18 +9,45 @@ import * as _ from 'lodash';
 })
 export class VenueAutoUpdateScheduleSwitcherComponent {
 
+    private _autoUpdateSchedule: AutoupdateSchedule;
+
+    @Input('autoUpdateSchedule')
+    set autoUpdateSchedule(value: AutoupdateSchedule) {
+        this._autoUpdateSchedule = value;
+        this.setUpAutoUpdateTime();
+    }
+
+    get autoUpdateSchedule(): AutoupdateSchedule {
+        return this._autoUpdateSchedule;
+    }
+
     public autoUpdateTime: { time: string, period: string } = {time: '08:00', period: 'AM'};
 
-    @Output() timeChange: EventEmitter<string> = new EventEmitter<string>();
+    @Output() change: EventEmitter<AutoupdateSchedule> = new EventEmitter<AutoupdateSchedule>();
 
     constructor(
     ) { }
 
+    setUpAutoUpdateTime(): void {
+        const cron = this._autoUpdateSchedule.eventTime;
+        let converter: CustomCronConverter = new CustomCronConverter(cron, CronConvertStrategy.PERIOD_SENSITIVE);
+
+        this.autoUpdateTime.time = converter.getTime();
+        this.autoUpdateTime.period = converter.getPeriod();
+    }
+
     onTimePeriodChange(period: string): void {
         this.autoUpdateTime.period = period;
+        this.notifyAutoUpdateConfigChanged();
     }
 
     onTimeChange(time: string): void {
         this.autoUpdateTime.time = time;
+        this.notifyAutoUpdateConfigChanged();
+    }
+
+    notifyAutoUpdateConfigChanged(): void {
+        console.log(this.autoUpdateTime);
+        console.log(this._autoUpdateSchedule);
     }
 }
