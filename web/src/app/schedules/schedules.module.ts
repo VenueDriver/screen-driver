95d4929a6bee8084c2b7e6c420e062f7099607e5
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-27 10:27:23 +0300

    Move datepicker component to the shared folder

--- web/src/app/schedules/schedules.module.ts
+++ web/src/app/schedules/schedules.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from "@angular/core";
-import {DatepickerModule} from "../datepicker/datepicker.module";
+import {DatepickerModule} from "../shared/datepicker/datepicker.module";
 import {SingleScheduleComponent} from "./single-schedule/single-schedule.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {AutocompleteModule} from "../autocomplete/autocomplete.module";
