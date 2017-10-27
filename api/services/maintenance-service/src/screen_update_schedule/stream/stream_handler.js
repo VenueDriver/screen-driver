641be6d7edd3079d45142969fd41e1d22e4fb3fc
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-27 16:06:26 +0300

    Fix typos in the variables names

--- api/services/maintenance-service/src/screen_update_schedule/stream/stream_handler.js
+++ api/services/maintenance-service/src/screen_update_schedule/stream/stream_handler.js
@@ -3,14 +3,14 @@
 const Notifier = require('../../notifier/notifier');
 const PusherChannels = require('../../notifier/pusher_chanels').PusherChannels;
 
-const venueChannel = PusherChannels.venues.name;
-const channelEvent = PusherChannels.venues.events.auto_update_schedule_updated;
+const VenuesChannel = PusherChannels.venues;
+const ChannelEvent = VenuesChannel.events.auto_update_schedule_updated;
 
 module.exports.handleEvent = (event, context, callback) => {
     event.Records.forEach((record) => {
         let newImage = record.dynamodb.NewImage;
         if (isNotRemoveAction(record.eventName)) {
-            Notifier.pushNotification(venueChannel, channelEvent, scheduleConfigFromImage(newImage));
+            Notifier.pushNotification(VenuesChannel.name, ChannelEvent, scheduleConfigFromImage(newImage));
         }
     });
 };
