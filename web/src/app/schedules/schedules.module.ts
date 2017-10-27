de120f7f5bab335351ba3d65ba4692ca2522734d
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-27 10:37:28 +0300

    Move checkbox-multiselector to the shared folder

--- web/src/app/schedules/schedules.module.ts
+++ web/src/app/schedules/schedules.module.ts
@@ -6,7 +6,7 @@ import {AutocompleteModule} from "../shared/autocomplete/autocomplete.module";
 import {SchedulesService} from "./schedules.service";
 import {SettingStateHolderService} from "../settings/setting-state-manager/settings-state-holder.service";
 import {SchedulesComponent} from "./schedules.component";
-import {CheckboxMultiselectorModule} from "../checkbox-multiselector/checkbox-multiselector.module";
+import {CheckboxMultiselectorModule} from "../shared/checkbox-multiselector/checkbox-multiselector.module";
 import { DaysOfWeekMultiselectorComponent } from './single-schedule/days-of-week-multiselector/days-of-week-multiselector.component';
 import {TimeSelectorModule} from "../shared/time-selector/time-selector.module";
 import {ToggleButtonModule} from "../shared/toggle-button/toggle-button.module";
