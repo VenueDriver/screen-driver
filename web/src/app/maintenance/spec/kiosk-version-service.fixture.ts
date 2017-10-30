49d8219a6f4434a4b1858cab20d8c7ba01eb6e63
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-30 11:58:33 +0200

    Add fixtures for kiosk app versions generation

---
+++ web/src/app/maintenance/spec/kiosk-version-service.fixture.ts
@@ -0,0 +1,15 @@
+import {KioskVersionDetails} from "../entities/kiosk-version-details";
+import * as _ from 'lodash';
+
+export class KioskVersionServiceFixture {
+
+    static kioskVersions(count: number): Array<KioskVersionDetails> {
+        return _.range(count).map((index) => {
+            const kioskVersion = new KioskVersionDetails();
+            kioskVersion.screenId = index;
+            kioskVersion.version = index;
+
+            return kioskVersion;
+        });
+    }
+}
