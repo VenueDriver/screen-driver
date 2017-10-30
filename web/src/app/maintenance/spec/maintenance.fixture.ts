629a623d22b49a62e85e756a7cf26907e2e94a58
volodymyro-in6k <volodymyro@interlink-ua.com>
2017-10-30 15:54:50 +0200

    Add fixture for venue creation

---
+++ web/src/app/maintenance/spec/maintenance.fixture.ts
@@ -0,0 +1,13 @@
+import * as _ from 'lodash';
+import {VenueMaintenanceInfo} from "../entities/venue-maintenance-info";
+
+export class MaintenanceFixture {
+
+    static venueMaintenanceInfo(count: number): Array<VenueMaintenanceInfo> {
+        return _.range(count).map((index) => {
+            const venue = new VenueMaintenanceInfo();
+            venue.id = index;
+            return venue;
+        });
+    }
+}
