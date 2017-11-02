7e0d9e9a3b692353d506155922b52399fae5b033
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-11-02 10:01:15 +0200

    Improve web app deploymet process

--- web/scripts/build.sh
+++ web/scripts/build.sh
@@ -2,7 +2,12 @@
 
 cd src/environments
 echo "Set up $API_HOST as host URL"
+cp environment.prod.ts environment.prod.ts.tmp
 sed -i 's@API_HOST@'"$API_HOST"'@' environment.prod.ts
 cd ../../
 
 ng build --prod --aot --no-sourcemap
+
+cd src/environments
+cp environment.prod.ts.tmp environment.prod.ts
+rm environment.prod.ts.tmp
