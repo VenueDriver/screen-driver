57789f6c1db164d6e8eb642a782a23545cf89c69
Artem Klots <artemkl@softwareplanetgroup.com>
2017-11-02 18:40:26 +0200

    Refactor forgot password API

--- api/services/auth-service/src/profile/password.js
+++ api/services/auth-service/src/profile/password.js
@@ -9,19 +9,24 @@ const parametersBuilder = require('./helpers/parameters_builder');
 
 module.exports.reset = (event, context, callback) => {
     const data = JSON.parse(event.body);
-    let params = parametersBuilder.buildFindUserByEmailParameters(data.email);
 
-    dbHelper.findByParams(params).then(users => {
-        if (users.length === 0) throw new Error('User does not exist');
-        let username = users[0].username;
-        return UserPool.resetPassword(username);
-    }).then(response => {
+    resetPassword(data.email).then(response => {
         callback(null, responseHelper.createSuccessfulResponse(response));
     }).catch(error => {
         console.log(error);
         callback(null, responseHelper.createResponseWithError(500, error.message));
     });
 };
+
+function resetPassword(email) {
+    let params = parametersBuilder.buildFindUserByEmailParameters(email);
+
+    return dbHelper.findByParams(params).then(users => {
+        if (users.length === 0) throw new Error('User does not exist');
+        let username = users[0].username;
+        return UserPool.resetPassword(username);
+    })
+}
 
 module.exports.confirmReset = (event, context, callback) => {
     const data = JSON.parse(event.body);
