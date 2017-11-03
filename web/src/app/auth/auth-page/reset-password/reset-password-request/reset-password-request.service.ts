7611cd56abbee755535e86a2d4459d505b4fad54
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-11-03 15:46:48 +0200

    Create service for sending reset password request

---
+++ web/src/app/auth/auth-page/reset-password/reset-password-request/reset-password-request.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+import * as AuthConsts from "../../../auth-consts";
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class ResetPasswordRequestService {
+
+    constructor(private httpClient: HttpClient) {
+
+    }
+
+    sendResetPasswordRequest(email: string): Observable<any> {
+        return this.httpClient.post(AuthConsts.RESET_PASSWORD_API, {email: email});
+    }
+}
