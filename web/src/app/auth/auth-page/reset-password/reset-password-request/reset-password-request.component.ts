f6a6c2e2bceea7a5aceca4184dca487bb51fa955
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-11-03 15:48:17 +0200

    Implement sending reset password request on form submit

--- web/src/app/auth/auth-page/reset-password/reset-password-request/reset-password-request.component.ts
+++ web/src/app/auth/auth-page/reset-password/reset-password-request/reset-password-request.component.ts
@@ -1,23 +1,41 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {AuthService} from "../../../auth.service";
+import {ResetPasswordRequestService} from "./reset-password-request.service";
 
 @Component({
     selector: 'reset-password-request',
     templateUrl: './reset-password-request.component.html',
-    styleUrls: ['../../auth.component.sass']
+    styleUrls: ['../../auth.component.sass'],
+    providers: [ResetPasswordRequestService]
 })
 export class ResetPasswordRequestComponent implements OnInit {
 
-    isRequestPerforming: false;
+    isRequestPerforming = false;
 
     @Input() email: string;
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService,
+                private resetPasswordService: ResetPasswordRequestService) {
     }
 
     ngOnInit() {
         this.authService.unauthorizedUserEmail.asObservable()
             .subscribe(email => this.email = email)
+    }
+
+    submitRequest() {
+        this.isRequestPerforming = true;
+        this.resetPasswordService.sendResetPasswordRequest(this.email)
+            .map(() => this.isRequestPerforming = false)
+            .subscribe(this.handleSuccessResponse, this.handleErrorResponse)
+    }
+
+    handleSuccessResponse(response) {
+        console.log(response)
+    }
+
+    handleErrorResponse(error) {
+        console.log(error)
     }
 
 }
