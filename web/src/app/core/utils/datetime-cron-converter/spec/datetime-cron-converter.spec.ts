ad13577b6634ca365708406a607c3e48a7356574
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-11-07 18:24:58 +0200

    Add tests for datetime cron converter

---
+++ web/src/app/core/utils/datetime-cron-converter/spec/datetime-cron-converter.spec.ts
@@ -0,0 +1,78 @@
+import {DatetimeToCronConverter} from "../datetime-cron.converter";
+
+fdescribe('DatetimeToCronConverter', () => {
+
+    describe('createCronForSpecificDate()', () => {
+
+        describe('when input is Oct 27, 2017', () => {
+            it('should return 0 0 0 27 OCT * 2017', () => {
+                let result = DatetimeToCronConverter.createCronForSpecificDate(new Date('Oct 27, 2017'));
+                expect(result).toBe('0 0 0 27 OCT * 2017');
+            });
+        });
+
+    });
+
+    describe('createCronForWeekDays()', () => {
+
+        describe('when input is WED', () => {
+            it('should return * * * * * WED', () => {
+                let result = DatetimeToCronConverter.createCronForWeekDays('WED');
+                expect(result).toBe('* * * * * WED');
+            });
+        });
+
+        describe('when input is SUN,MON', () => {
+            it('should return * * * * * SUN,MON', () => {
+                let result = DatetimeToCronConverter.createCronForWeekDays('SUN,MON');
+                expect(result).toBe('* * * * * SUN,MON');
+            });
+        });
+
+        describe('when input is SUN,MON,TUE,WED,THU,FRI,SAT', () => {
+            it('should return * * * * * SUN,MON,TUE,WED,THU,FRI,SAT', () => {
+                let result = DatetimeToCronConverter.createCronForWeekDays('SUN,MON,TUE,WED,THU,FRI,SAT');
+                expect(result).toBe('* * * * * SUN,MON,TUE,WED,THU,FRI,SAT');
+            });
+        });
+
+        describe('when input is MON,TUE,WD', () => {
+            it('should throw an error', () => {
+                expect(DatetimeToCronConverter.createCronForWeekDays.bind('MON,TUE,WD')).toThrowError();
+            });
+        });
+
+        describe('when input is MON.TUE.WED', () => {
+            it('should throw an error', () => {
+                expect(DatetimeToCronConverter.createCronForWeekDays.bind('MON.TUE.WED')).toThrowError();
+            });
+        });
+
+    });
+
+    describe('setTimeForCron()', () => {
+
+        describe('when input is \'0 0 0 27 OCT * 2017\', 12, 20', () => {
+            it('should return 0 20 12 27 OCT * 2017', () => {
+                let result = DatetimeToCronConverter.setTimeForCron('0 0 0 27 OCT * 2017', 12, 20);
+                expect(result).toBe('0 20 12 27 OCT * 2017');
+            });
+        });
+
+        describe('when input is \'0 0 0 27 OCT * 2017\', 24, 20', () => {
+            it('should return 0 20 0 27 OCT * 2017', () => {
+                let result = DatetimeToCronConverter.setTimeForCron('0 0 0 27 OCT * 2017', 24, 20);
+                expect(result).toBe('0 20 0 27 OCT * 2017');
+            });
+        });
+
+        describe('when input is \'0 0 0 27 OCT * 2017\', 9, 120', () => {
+            it('should return 0 0 11 27 OCT * 2017', () => {
+                let result = DatetimeToCronConverter.setTimeForCron('0 0 0 27 OCT * 2017', 9, 120);
+                expect(result).toBe('0 0 11 27 OCT * 2017');
+            });
+        });
+
+    });
+
+});
