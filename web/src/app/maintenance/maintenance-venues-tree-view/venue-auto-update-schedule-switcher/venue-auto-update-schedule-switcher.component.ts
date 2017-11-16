77f95f829d644d81e9d1cae22cf990ecb9dc7b9f
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-11-16 19:03:46 +0200

    Add save and cansel buttons that appear when a user chenges time

--- web/src/app/maintenance/maintenance-venues-tree-view/venue-auto-update-schedule-switcher/venue-auto-update-schedule-switcher.component.ts
+++ web/src/app/maintenance/maintenance-venues-tree-view/venue-auto-update-schedule-switcher/venue-auto-update-schedule-switcher.component.ts
@@ -3,6 +3,8 @@ import {AutoupdateSchedule} from "../../entities/autoupdate-schedule";
 import {VenueAutoUpdateScheduleSwitcherService} from "./venue-auto-update-schedule-switcher.service";
 import {VenueScheduleTimeSelectorParams} from "./time-selector-params.interface";
 
+import * as _ from 'lodash';
+
 @Component({
     selector: 'auto-update-schedule-switcher',
     templateUrl: 'venue-auto-update-schedule-switcher.component.html',
@@ -10,8 +12,11 @@ import {VenueScheduleTimeSelectorParams} from "./time-selector-params.interface"
     providers: [VenueAutoUpdateScheduleSwitcherService]
 })
 export class VenueAutoUpdateScheduleSwitcherComponent {
+
     public autoUpdateTime: VenueScheduleTimeSelectorParams;
+    private _originalAutoUpdateTime: VenueScheduleTimeSelectorParams;
     private _autoUpdateSchedule: AutoupdateSchedule;
+    private _timeChanged = false;
 
     @Input('autoUpdateSchedule')
     set autoUpdateSchedule(value: AutoupdateSchedule) {
@@ -23,7 +28,8 @@ export class VenueAutoUpdateScheduleSwitcherComponent {
         return this._autoUpdateSchedule;
     }
 
-    @Output() autoUpdateChange: EventEmitter<AutoupdateSchedule> = new EventEmitter<AutoupdateSchedule>();
+    @Output()
+    autoUpdateChange: EventEmitter<AutoupdateSchedule> = new EventEmitter<AutoupdateSchedule>();
 
     constructor(private service: VenueAutoUpdateScheduleSwitcherService) {
         this.autoUpdateTime = this.service.getDefaultAutoUpdateTime();
@@ -32,16 +38,17 @@ export class VenueAutoUpdateScheduleSwitcherComponent {
     setUpAutoUpdateTime(): void {
         const cron = this._autoUpdateSchedule.eventTime;
         this.autoUpdateTime = this.service.getTimeFromCron(cron);
+        this._originalAutoUpdateTime = _.cloneDeep(this.autoUpdateTime);
     }
 
     onTimePeriodChange(period: string): void {
         this.autoUpdateTime.period = period;
-        this.notifyAutoUpdateConfigChanged();
+        this.isCopyEqualToSource();
     }
 
     onTimeChange(time: string): void {
         this.autoUpdateTime.time = time;
-        this.notifyAutoUpdateConfigChanged();
+        this.isCopyEqualToSource();
     }
 
     onEnabledChange(isEnabled): void {
@@ -64,4 +71,21 @@ export class VenueAutoUpdateScheduleSwitcherComponent {
     get isTimeSelectDisabled(): boolean {
         return !this.isEnabled;
     }
+
+    performSubmit() {
+        event.stopPropagation();
+        this._timeChanged = false;
+        this.notifyAutoUpdateConfigChanged();
+    }
+
+    performCancel(event: any) {
+        event.stopPropagation();
+        this.autoUpdateTime = _.cloneDeep(this._originalAutoUpdateTime);
+        this._timeChanged = false;
+    }
+
+    isCopyEqualToSource() {
+        this._timeChanged = !_.isEqual(this.autoUpdateTime, this._originalAutoUpdateTime);
+    }
+
 }
