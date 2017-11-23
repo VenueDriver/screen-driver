bc62608addcd27ddbcb2d0803624d53d850ae629
Artem Klots <artemkl@softwareplanetgroup.com>
2017-11-23 14:14:54 +0200

    Fix compilation errors after updating to Angular 5, by removing deprecated code usage

--- web/src/app/maintenance/autoupdate-schedule.service.ts
+++ web/src/app/maintenance/autoupdate-schedule.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import {AutoupdateSchedule} from "./entities/autoupdate-schedule";
 
 import * as _ from 'lodash';
+import {ApiService} from "../shared/services/api.service";
 
 const EVERY_DAY_AT_ONE_AM_CRON_EXPRESSION = '0 0 1 * * * *';
 
@@ -13,14 +13,14 @@ export class AutoupdateScheduleService {
 
     readonly apiPath = `${environment.apiUrl}/api/screens/update-schedule`;
 
-    constructor(private httpClient: HttpClient) { }
+    constructor(private apiService: ApiService) { }
 
     loadAutoupdateSchedule(): Observable<Array<AutoupdateSchedule>> {
-        return this.httpClient.get(this.apiPath);
+        return this.apiService.get(this.apiPath);
     }
 
     upsert(schedule: AutoupdateSchedule): Observable<AutoupdateSchedule> {
-        return this.httpClient.put(this.apiPath, schedule);
+        return this.apiService.put(this.apiPath, schedule);
     }
 
     createDefaultAutoapdateSchedule(): AutoupdateSchedule {
