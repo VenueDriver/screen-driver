036b10cfb43237756a837ef353ce491b1532338b
Artem Klots <artemkl@softwareplanetgroup.com>
2017-11-23 16:24:24 +0200

    Add missed services in failed tests

--- web/src/app/maintenance/spec/autoupdate-shedule.service.spec.ts
+++ web/src/app/maintenance/spec/autoupdate-shedule.service.spec.ts
@@ -8,10 +8,13 @@ import {
 } from "@angular/http";
 import {MockBackend} from "@angular/http/testing";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
-import {HttpClient, HttpClientModule, HttpParams, HttpRequest} from "@angular/common/http";
+import {HttpClient, HttpClientModule, HttpRequest} from "@angular/common/http";
 import {AutoupdateScheduleService} from "../autoupdate-schedule.service";
 import {AutoupdateScheduleServiceFixture} from "./fixtures/autoupdate-schedule-service.fixture";
 import {AutoupdateSchedule} from "../entities/autoupdate-schedule";
+import {ApiService} from "../../shared/services/api.service";
+import {SpinnerService} from "../../shared/spinner/spinner.service";
+import {DataLoadingMonitorService} from "../../shared/services/data-loading-monitor/data-loading-monitor.service";
 
 describe('Service: AutoupdateScheduleService', () => {
     let autoupdateScheduleService;
@@ -20,14 +23,21 @@ describe('Service: AutoupdateScheduleService', () => {
             imports: [HttpClientModule, HttpClientTestingModule],
             providers: [
                 AutoupdateScheduleService,
+                ApiService,
+                SpinnerService,
+                DataLoadingMonitorService,
                 {provide: ConnectionBackend, useClass: MockBackend},
                 {provide: RequestOptions, useClass: BaseRequestOptions},
             ]
         });
     });
 
-    beforeEach(inject([AutoupdateScheduleService, ConnectionBackend], (autoupdateScheduleService) => {
+    beforeEach(inject([AutoupdateScheduleService, ApiService, SpinnerService, DataLoadingMonitorService, ConnectionBackend],
+        (autoupdateScheduleService, apiService, spinnerService, dataLoadingMonitorService) => {
         this.autoupdateScheduleService = autoupdateScheduleService;
+        this.apiService = apiService;
+        this.spinnerService = spinnerService;
+        this.dataLoadingMonitorService = dataLoadingMonitorService;
     }));
 
     describe('apiPath', () => {
