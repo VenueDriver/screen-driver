759f794ea89c3d02ec9f021984c73373c08299fb
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-12-05 18:05:26 +0200

    Implement retreiving user email from URL reset password

--- web/src/app/auth/auth-page/reset-password/reset-password.component.ts
+++ web/src/app/auth/auth-page/reset-password/reset-password.component.ts
@@ -16,6 +16,7 @@ export class ResetPasswordComponent implements OnInit {
     ngOnInit() {
         this.activatedRoute.queryParams.subscribe(params => {
             this.verificationCode = params['token'];
+            this.userEmail = params['email']
         });
     }
 
