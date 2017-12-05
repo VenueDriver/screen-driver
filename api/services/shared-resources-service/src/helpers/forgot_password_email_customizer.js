a37fc8a47a481dc330c7d2eaed7edae98bc74c06
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-12-05 14:56:32 +0200

    Implement reset password email customization

--- api/services/shared-resources-service/src/helpers/forgot_password_email_customizer.js
+++ api/services/shared-resources-service/src/helpers/forgot_password_email_customizer.js
@@ -1,7 +1,34 @@
 class ForgotPasswordEmailCustomizer {
 
     static customize(event) {
-        console.log(event)
+        let clientAppUrl = ForgotPasswordEmailCustomizer._getSignUpUrl();
+        event.response.emailSubject = "[ScreenDriver] Your password reset verification code";
+        event.response.emailMessage = ForgotPasswordEmailCustomizer._getMessageTemplate(clientAppUrl);
+    }
+
+    static _getSignUpUrl() {
+        let clientAppUrl = process.env.STAGE === 'production' ? process.env.CLIENT_APP_URL : process.env.CLIENT_APP_STAGING_URL;
+        return `${clientAppUrl}/#/auth/reset-password/?token=`;
+    }
+
+    static _getMessageTemplate(clientAppUrl) {
+        return `
+            <div>
+                Hello,
+                <br><br>
+                You've recently requested for resetting the password.
+                <br><br>
+                Use the following link to proceed reset password operation:
+                <br><br>
+                ${clientAppUrl}{####}
+                <br><br>
+                Please contact your system administrator in case of any issues with password reset.
+                <br><br>
+                Best regards,
+                <br>
+                ScreenDriver team
+            </div>
+        `
     }
 
 }
