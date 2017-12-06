c739b547e9e414a5240496365ee31bd10c62fca3
Artem Klots <artemkl@softwareplanetgroup.com>
2017-12-06 10:28:21 +0200

    Move notification listener to helpers. Rename error directory to errors

--- kiosk/src/js/services/services_initialiser.js
+++ kiosk/src/js/services/services_initialiser.js
@@ -4,9 +4,9 @@ class ServicesInitialiser {
     //should be called ON application initializing
     static initBaseServices() {
         require('./logger/logger').setupLoggerProperties();
-        require('./error/global_errors_handling_service');
-        require('./error/network_errors_handling_service');
-        require('./error/uncaught_errors_handling_service');
+        require('./errors/global_errors_handling_service');
+        require('./errors/network_errors_handling_service');
+        require('./errors/uncaught_errors_handling_service');
         require('./network/connection_status_service');
         require('./data/autoupdate_schedule_watcher');
         require('./data/server_data_watcher');
