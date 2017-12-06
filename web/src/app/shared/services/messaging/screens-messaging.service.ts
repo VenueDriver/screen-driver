decbc01a40bf398fe6b06e44b1553197cf4537b0
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-12-06 11:45:14 +0200

    Refactor code. Create variables for API endpoints instead of using interpolation

--- web/src/app/shared/services/messaging/screens-messaging.service.ts
+++ web/src/app/shared/services/messaging/screens-messaging.service.ts
@@ -5,18 +5,20 @@ import {ApiService} from "../api.service";
 
 @Injectable()
 export class ScreensMessagingService {
-    readonly screensApiPath = '/api/screens';
+
+    readonly refreshScreensApiUrl = '/api/screens/refresh';
+    readonly updateApplicationsApiPath = '/api/screens/update-applications';
 
     constructor(private apiService: ApiService, private notificationService: NotificationService) {
     }
 
     refreshScreen(id: string): Observable<any> {
         let data = {screens: [id]};
-        return this.apiService.post(`${this.screensApiPath}/refresh`, data);
+        return this.apiService.post(this.refreshScreensApiUrl, data);
     }
 
     updateClientApps(content: any) {
-        return this.apiService.post(`${this.screensApiPath}/update-applications`, content).subscribe(
+        return this.apiService.post(this.updateApplicationsApiPath, content).subscribe(
             () => this.notificationService.showSuccessNotificationBar('Update request has been sent successfully.'),
             (error) => this.notificationService.showErrorNotificationBar('Update request failed. Try again after refresh the page.'))
     };
