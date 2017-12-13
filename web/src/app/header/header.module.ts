8f8d9821e32ca0581a8ee6615877bafb345f4c68
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-12-13 15:09:40 +0200

    Use hide on scroll directive on the header component, remove unused methods as well as hostlistener from the header component

--- web/src/app/header/header.module.ts
+++ web/src/app/header/header.module.ts
@@ -3,11 +3,13 @@ import {HeaderComponent} from "./header.component";
 import {HeaderService} from "./header.service";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "../app-routing.module";
+import {HideOnScrollModule} from "../shared/directives/hide-on-scroll/hide-on-scroll.module";
 
 @NgModule({
     imports: [
         AppRoutingModule,
         BrowserModule,
+        HideOnScrollModule,
     ],
     declarations: [
         HeaderComponent,
