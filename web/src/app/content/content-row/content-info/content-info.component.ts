64e54cf866cdf25cb5e7db685cbc7fefe4f5d8ba
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-12-14 12:34:57 +0200

    Fix content list styles for desctop

--- web/src/app/content/content-row/content-info/content-info.component.ts
+++ web/src/app/content/content-row/content-info/content-info.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Content} from "../../content";
 import {DeviceSizeChecker} from "../../../shared/utils/device-size-checker";
 
-const MAX_DISPLAYING_URL_LENGTH = DeviceSizeChecker.isTablet() ? 70 : 90;
+const MAX_DISPLAYING_URL_LENGTH = DeviceSizeChecker.isTablet() ? 70 : 150;
 
 @Component({
     selector: 'content-info',
