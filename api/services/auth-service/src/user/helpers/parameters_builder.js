8756f4539bc0e997462a35f90b04a3e1ef50aef8
Vitalii Kobrin <vitaliik@softwareplanet.uk.com>
2017-12-23 18:03:33 +0200

    Build create user request and specify item fields which should be saved

--- api/services/auth-service/src/user/helpers/parameters_builder.js
+++ api/services/auth-service/src/user/helpers/parameters_builder.js
@@ -27,7 +27,13 @@ module.exports.buildUpdateRequestParameters = (user) => {
 module.exports.buildCreateRequestParameters = (user) => {
     return {
         TableName: process.env.USERS_TABLE,
-        Item: user,
+        Item: {
+            id: user.id,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            enabled: user.enabled,
+            _rev: 0
+        },
     }
 };
 
